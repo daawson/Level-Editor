dc0b26df40b0a0a161a5d2f2e8d7b97207b2d182
Daawson <dkujawskiii@gmail.com>
2017-09-25 20:42:11 +0200

    Update
    Start przerzucania głównych funkcji na jedno okno.

---
+++ LevelEditor/CMenu/OtterMenu.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Otter;
+
+namespace CMenu
+{
+    class OtterMenu:Entity
+    {
+        public static OtterMenu Instance = new OtterMenu();
+
+        Image mainbg;
+        Image topbar;
+
+        Vector2 cameraPos;
+
+        enum windowCollider
+        {
+            TopBar
+        }
+
+        private OtterMenu()
+        {
+            InitWindow(100, 500, Color.White);
+            Layer = -1;
+        }
+
+        public void InitWindow(int w, int h) {
+            InitWindow(w, h, Color.Cyan);
+        }
+
+        public void InitWindow(int w, int h, Color c)
+        {
+            mainbg = Image.CreateRectangle(w, h, c);
+            topbar = Image.CreateRectangle(w, 20, Color.Black);
+
+            AddGraphics(mainbg);
+            AddGraphics(topbar);
+            SetPosition(0, 0);
+        }
+
+        void FollowCamera()
+        {
+            SetPosition(cameraPos);
+        }
+
+        void FollowMouse(Vector2 mPos)
+        {
+            mainbg.SetPosition(mPos.X - mainbg.HalfWidth, mPos.Y - 10);
+            topbar.SetPosition(mPos.X - topbar.HalfWidth, mPos.Y - 10);
+
+            //mainbg.SetOrigin(mPos);
+            //topbar.SetOrigin(mPos);
+        }
+
+        void CheckDragDrop()
+        {
+            Vector2 mPos = new Vector2(Scene.Instance.Input.MouseScreenX, Scene.Instance.Input.MouseScreenY);
+            bool isDragging = Game.Instance.Input.MouseButtonDown(MouseButton.Left) ? true : false;
+            if (isDragging && isVectorInside(mPos, topbar.X, topbar.Y, topbar.Width, topbar.Height))
+            {
+                Console.WriteLine("Drag: {0}", isDragging);
+                FollowMouse(mPos);
+            }
+        }
+        public override void Update()
+        {
+            base.Update();
+            CheckDragDrop();
+        }
+
+        public override void UpdateLast()
+        {
+            base.UpdateLast();
+            cameraPos = new Vector2(Scene.Instance.CameraX, Scene.Instance.CameraY);
+            FollowCamera();
+            CheckDragDrop();
+        }
+
+        public bool isVectorInside(Vector2 pos, float x, float y, float w, float h)
+        {
+            if (pos.X > x && pos.X < x + w && pos.Y > y && pos.Y < y + h) return true;
+            else return false;
+        }
+    }
+}
---
+++ LevelEditor/Handler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Otter;
+using System.Windows.Forms;
+
+namespace LevelEditor
+{
+    class Handler
+    {
+        public Scene usedScene;
+        OpenFileDialog openFileDialog1;
+        public Handler(Scene s)
+        {
+            openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Title = "Wybierz plik tilesetu: ";
+            openFileDialog1.Filter = "PNG Files | *.png";
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                if (openFileDialog1.FileName == null)
+                {
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Image file = new Image(openFileDialog1.FileName);
+
+                    usedScene = s;
+                    usedScene.Add(new Akcja(usedScene, openFileDialog1.FileName, file));
+                }
+            }
+            else if(result == DialogResult.None || result == DialogResult.Cancel || result == DialogResult.No)
+            {
+                Environment.Exit(0);
+            }
+        }
+    }
+}
